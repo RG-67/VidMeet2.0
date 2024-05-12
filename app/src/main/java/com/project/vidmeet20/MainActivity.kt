686bf7596392e73ad25dedc93cbdaccea9989da8
Rittik <rittikghoshkolkata@gmail.com>
2024-05-12 11:49:54 +0530

    initial commit

--- app/src/main/java/com/project/vidmeet20/MainActivity.kt
+++ app/src/main/java/com/project/vidmeet20/MainActivity.kt
@@ -5,16 +5,32 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.project.vidmeet20.databinding.ActivityMainBinding
+import io.antmedia.webrtcandroidframework.api.IWebRTCClient
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
+        }*/
+
+        val webRtcClient: IWebRTCClient = IWebRTCClient.builder()
+            .setActivity(this)
+            .setLocalVideoRenderer(binding.surfaceRender)
+            .setServerUrl("wss://test.antmedia.io:5443/WebRTCAppEE/websocket")
+            .build()
+//        webRtcClient.publish("StreamRTK")
+        webRtcClient.play("stream1")
+
+
     }
 }
