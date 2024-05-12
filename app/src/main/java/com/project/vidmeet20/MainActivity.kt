7146f2fb88767f2b7d9c931fd9f5a28a1864a9d6
Rittik <rittikghoshkolkata@gmail.com>
2024-05-12 13:29:46 +0530

    initial commit

--- app/src/main/java/com/project/vidmeet20/MainActivity.kt
+++ app/src/main/java/com/project/vidmeet20/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.project.vidmeet20
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 import com.project.vidmeet20.databinding.ActivityMainBinding
 import io.antmedia.webrtcandroidframework.api.IWebRTCClient
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var webRtcClient: IWebRTCClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,15 +25,46 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }*/
-
-        val webRtcClient: IWebRTCClient = IWebRTCClient.builder()
+        webRtcClient = IWebRTCClient.builder()
             .setActivity(this)
             .setLocalVideoRenderer(binding.surfaceRender)
             .setServerUrl("wss://test.antmedia.io:5443/WebRTCAppEE/websocket")
             .build()
-//        webRtcClient.publish("StreamRTK")
-        webRtcClient.play("stream1")
 
-
+        setClick()
     }
+
+    private fun setClick() {
+        binding.startBtn.setOnClickListener {
+            if (binding.edtID.text.toString() == "") {
+                binding.edtID.error = "Enter ID"
+                Snackbar.make(it, "Enter ID", Snackbar.LENGTH_SHORT).show()
+            } else {
+                binding.stAndPlLin.visibility = View.GONE
+                binding.surfaceRender.visibility = View.VISIBLE
+                webRtcClient = IWebRTCClient.builder()
+                    .setActivity(this)
+                    .setLocalVideoRenderer(binding.surfaceRender)
+                    .setServerUrl("wss://test.antmedia.io:5443/WebRTCAppEE/websocket")
+                    .build()
+                webRtcClient.publish(binding.edtID.text.toString())
+            }
+        }
+        binding.playBtn.setOnClickListener {
+            if (binding.edtID.text.toString() == "") {
+                binding.edtID.error = "Enter ID"
+                Snackbar.make(it, "Enter ID", Snackbar.LENGTH_SHORT).show()
+            } else {
+                binding.stAndPlLin.visibility = View.GONE
+                binding.surfaceRender.visibility = View.VISIBLE
+                webRtcClient = IWebRTCClient.builder()
+                    .setActivity(this)
+                    .addRemoteVideoRenderer(binding.surfaceRender)
+                    .setServerUrl("wss://test.antmedia.io:5443/WebRTCAppEE/websocket")
+                    .build()
+                webRtcClient.play(binding.edtID.text.toString())
+            }
+        }
+    }
+
 }
